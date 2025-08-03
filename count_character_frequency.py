6cabc8fa41eeb9aeda1f974268714b851e68e80e
Abhishek.k <abhiabhishekk994@gmail.com>
2025-08-03 19:52:48 +0530

    count_character_frequency.py

---
+++ count_character_frequency.py
@@ -0,0 +1,8 @@
+def char_frequency(s):
+    freq = {}
+    for char in s:
+        if char != ' ':
+            freq[char] = freq.get(char, 0) + 1
+    return freq
+
+print(char_frequency("hello world"))
