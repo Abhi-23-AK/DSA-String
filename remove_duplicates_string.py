aeb8797b6ecacb9d060b273889302d04eeded7f0
Abhishek.k <abhiabhishekk994@gmail.com>
2025-08-05 20:06:47 +0530

    remove_duplicates_string.py

---
+++ remove_duplicates_string.py
@@ -0,0 +1,10 @@
+def remove_duplicates(s):
+    result = ""
+    seen = set()
+    for char in s:
+        if char not in seen:
+            result += char
+            seen.add(char)
+    return result
+
+print(remove_duplicates("programming"))  # "progamin"
